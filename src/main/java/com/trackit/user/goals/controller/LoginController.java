0b84047d1cf41c33c182263b2889da46dc6f9f45
sherlock <sherlock@bhargavs-mbp.home>
2018-04-06 22:09:20 -0400

    testing

--- src/main/java/com/trackit/user/goals/controller/LoginController.java
+++ src/main/java/com/trackit/user/goals/controller/LoginController.java
@@ -1,18 +1,19 @@
 package com.trackit.user.goals.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value ="/", method = RequestMethod.GET)
 public class LoginController {
 
 	
-@RequestMapping(value="/hello")
-public String Welcome(){
-	return "welcome";	
-} 
+	@RequestMapping(value ="/", method = RequestMethod.GET)
+	public String helloController(ModelMap model){
+		model.put("name","bhargav");
+		return "welcome";	
+	} 
 
 
 
