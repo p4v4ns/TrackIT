8402f981dc3d19e81fc2f4697e3829705567bc07
sherlock <sherlock@bhargavs-mbp.home>
2018-04-08 02:08:54 -0400

    Sample post to AWS RDS. Adding an Goal object.

--- src/main/java/com/trackit/user/goals/controller/TrackITController.java
+++ src/main/java/com/trackit/user/goals/controller/TrackITController.java
@@ -3,10 +3,13 @@ package com.trackit.user.goals.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.trackit.user.goals.dao.GoalRepository;
+import com.trackit.user.goals.models.Goal;
 
 @RestController
 public class TrackITController{
@@ -17,8 +20,29 @@ public class TrackITController{
 	
 	@GetMapping("/hello")
     public String sayHello(@RequestParam("id") String id) {
-		repository.findAll();
+		Goal goal = new Goal();
 		System.err.println(repository.count() +" " +repository.findAll() +" " +repository.findByName("wow").get(0).getName());
         return  id;
+    } 
+	
+	@PostMapping("/add")
+    public String addGoal(@RequestBody Goal goal) {
+		String msg = "";
+		System.err.println();
+		if(null != goal){
+			repository.save(goal);	
+		}
+		else{
+			msg = "please pass the goal object{ name:goal , type:type}";
+		}
+		
+		if(null == repository.findByName(goal.getName()) || repository.findByName(goal.getName()).isEmpty()){
+			msg = "goal not added please check the goal details again";
+		}
+		else{
+			msg = repository.findByName(goal.getName()).get(0).getName() + " has been added sucessfully";
+		}
+		
+        return  msg;
     } 
 }
--- src/main/java/com/trackit/user/goals/models/Goal.java
+++ src/main/java/com/trackit/user/goals/models/Goal.java
@@ -9,7 +9,6 @@ import javax.persistence.Table;
 
 
 @Entity
-@Table(name="Goal")
 public class Goal {
 	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
