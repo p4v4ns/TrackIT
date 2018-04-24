863f0cb06a8d1e5e247b929890e86331b0b2e197
varadhi <varadhi@10.0.0.127>
2018-04-23 23:22:40 -0400

    Gets all the goals to the user with user_id.

--- src/main/java/com/trackit/user/goals/models/Goal.java
+++ src/main/java/com/trackit/user/goals/models/Goal.java
@@ -10,12 +10,14 @@ import javax.persistence.Table;
 
 @Entity
 public class Goal {
+	
 	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="id")
 	private long id;
 	private String name;
 	private String type;
+	private int user_identifier;
 	 
 	public long getId() {
 		return id;
@@ -35,7 +37,11 @@ public class Goal {
 	public void setType(String type) {
 		this.type = type;
 	}
-	 
-	 
-	 
+	public int getUser_identifier() {
+		return user_identifier;
+	}
+	public void setUser_identifier(int user_identifier) {
+		this.user_identifier = user_identifier;
+	}
+	
 }
